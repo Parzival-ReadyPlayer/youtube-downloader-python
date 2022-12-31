c169ab27ba3ae04b942a8726bc476e6e865766c1
Parzival-ReadyPlayer <nicco.girotti@gmail.com>
2022-12-31 13:43:54 -0300

    cambios en download.html y home

--- youtube_app.py
+++ youtube_app.py
@@ -19,77 +19,10 @@ class linkForm(FlaskForm):
     link = StringField('Youtube link', validators=[DataRequired()])
     submit = SubmitField('Descargar')
     
-# Ruta audio
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    # Create form
-    form = linkForm()
-    if request.method == "POST":
-        flash('Iniciando descarga, espere por favor..', 'warning')
-        # Get link from form
-        link = request.form.get('link')
-        while True:
-            # Try catch block, if the input is not valid we print a error message
-            try:
-                # Call function
-                download_audio(link)
-                flash('Download succesfull', 'success')
-                return redirect(url_for('index'))
-            except:
-                flash('That is an invalid link', 'danger')
-                return redirect(url_for('index'))
-    return render_template('index.html', form = form)
-
-
-# Ruta playlist
-
-@app.route('/playlist', methods=['GET', 'POST'])
-def playlist():
-    form = linkForm()
-    if request.method == 'POST':
-        link = request.form.get('link')
-        while True:
-            try:
-                download_playlist(link)
-                flash('Download complete', 'success')
-                return redirect('playlist')
-            except:
-                flash('That is an invalid link for a playlist', 'danger')
-                return redirect('playlist')
-            
-    return render_template('playlist.html', form=form)
-
-
-# Ruta video
-
-@app.route('/video', methods=['GET', 'POST'])
-def video():
-    # Create form
-    form = linkForm()
-    if request.method == "POST":
-        # Get link from form
-        link = request.form.get('link')
-        while True:
-            # Try catch block, if the input is not valid we print a error message
-            try:
-                # Call function
-                download_video(link)
-                flash('Download succesfull', 'success')
-                return redirect(url_for('video'))
-            except:
-                flash('That is an invalid link', 'danger')
-                return redirect(url_for('video'))
-    return render_template('video.html', form = form)
-
-
-
 
 
 @app.route("/home", methods = ["GET", "POST"])
 def home():
-    
     form = linkForm()
     if request.method == "POST":
         session['link'] = request.form.get('link')
@@ -102,18 +35,7 @@ def home():
         return render_template("download.html", url = url, form = form)
     return render_template("home.html", form=form)
 
-@app.route("/download", methods = ["GET", "POST"])
-def download_video():
-    if request.method == "POST":
-        buffer = BytesIO()
-        url = YouTube(session['link'])
-        name = url.title
-        itag = request.form.get("itag")
-        video = url.streams.get_by_itag(itag)
-        video.stream_to_buffer(buffer)
-        buffer.seek(0)
-        return send_file(buffer, as_attachment=True, download_name=name, mimetype="video/mp4")
-    return redirect(url_for("home"))
+
 
 
 @app.route("/download_audio", methods = ["GET", "POST"])
@@ -121,12 +43,26 @@ def download_audio():
     if request.method == "POST":
         buffer = BytesIO()
         url = YouTube(session['link'])
-        name = url.title
+        name = url.title + '.mp3'
         audio = url.streams.get_audio_only()
         audio.stream_to_buffer(buffer)
         buffer.seek(0)
-        return send_file(buffer, as_attachment=True, download_name=name, mimetype="audio/mp3")
+        return send_file(buffer, as_attachment=True, download_name=name)
     return redirect(url_for("home"))
+
+@app.route("/download_video", methods = ["GET", "POST"])
+def download_video():
+    if request.method == "POST":
+        buffer = BytesIO()
+        url = YouTube(session['link'])
+        name = url.title
+        itag = request.form.get('itag')
+        video = url.streams.get_by_itag(itag)
+        video.stream_to_buffer(buffer)
+        buffer.seek(0)
+        return send_file(buffer, as_attachment=True, download_name=name, mimetype='video/mp4')
+    return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
